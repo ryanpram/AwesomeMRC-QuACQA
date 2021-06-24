770417597b309826609770aa41ba7a9992602971
ryan <ryan46pramana@gmail.com>
2021-06-24 18:31:37 +0700

    Add: test

--- transformer-mrc/examples/run_squad_av.py
+++ transformer-mrc/examples/run_squad_av.py
@@ -16,6 +16,8 @@
 """ Finetuning the library models for question-answering on SQuAD (DistilBERT, Bert, XLM, XLNet)."""
 
 from __future__ import absolute_import, division, print_function
+from transformers import AdamW, get_linear_schedule_with_warmup
+
 from transformers.data.processors.squad import SquadV1Processor, SquadV2Processor, SquadResult
 from transformers.data.metrics.squad_metrics import compute_predictions_logits, compute_predictions_log_probs, squad_evaluate
 from examples.evaluate_official2 import eval_squad
