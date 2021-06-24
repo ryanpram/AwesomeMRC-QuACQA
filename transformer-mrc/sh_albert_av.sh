8912fcc838c1518ccb0491668a40bdca7fdfeeba
ryan <ryan46pramana@gmail.com>
2021-06-24 18:12:40 +0700

    Update: sh albert av

--- transformer-mrc/sh_albert_av.sh
+++ transformer-mrc/sh_albert_av.sh
@@ -1,25 +1,25 @@
 #intensive module
-export SQUAD_DIR=data/
+export SQUAD_DIR=../data
 python ./examples/run_squad_av.py \
     --model_type albert \
-    --model_name_or_path albert-xxlarge-v2 \
+    --model_name_or_path albert-base-v2 \
     --do_train \
     --do_eval \
     --do_lower_case \
     --version_2_with_negative \
-    --train_file $SQUAD_DIR/train-v2.0.json \
-    --predict_file $SQUAD_DIR/dev-v2.0.json \
+    --train_file $SQUAD_DIR/quac_train.json \
+    --predict_file $SQUAD_DIR/quac_val.json \
     --learning_rate 2e-5 \
     --num_train_epochs 2 \
     --max_seq_length 512 \
     --doc_stride 128 \
     --max_query_length=64 \
-    --per_gpu_train_batch_size=6 \
-    --per_gpu_eval_batch_size=8 \
+    --per_gpu_train_batch_size=8 \
+    --per_gpu_eval_batch_size=6 \
+    --gradient_accumulation_steps=4 \
     --warmup_steps=814 \
-    --output_dir squad/squad2_albert-xxlarge-v2_lr2e-5_len512_bs48_ep2_wm814_av_ce_fp16 \
+    --output_dir squad/squad2_albert-base-v2_lr2e-5_len512_bs32_ep2_wm814_av_ce \
     --eval_all_checkpoints \
-    --save_steps 2500 \
+    --save_steps 1000 \
     --n_best_size=20 \
     --max_answer_length=30 \
-    --fp16
