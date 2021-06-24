62fc4be186c59b5ca1b269833b7e552366267df4
ryan <ryan46pramana@gmail.com>
2021-06-24 21:20:05 +0700

    Add: a

---
+++ transformer-mrc/sh_albert_cls_2.sh
@@ -0,0 +1,22 @@
+#sketchy module
+export DATA_DIR=../data
+export TASK_NAME=squad
+CUDA_VISIBLE_DEVICES=$1 \
+python3 ./run_cls.py \
+    --model_type albert \
+    --model_name_or_path albert-base-v2 \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_eval \
+    --do_lower_case \
+    --data_dir $DATA_DIR \
+    --max_seq_length 512 \
+    --per_gpu_train_batch_size=8   \
+    --gradient_accumulation_steps=4 \
+    --per_gpu_eval_batch_size=6   \
+    --warmup_steps=814 \
+    --learning_rate 3e-5 \
+    --num_train_epochs 2.0 \
+    --eval_all_checkpoints \
+    --save_steps 2500 \
+    --output_dir squad/cls_squad2_albert-base-v2_lr3e-5_len512_bs32_ep2_wm814_f$
